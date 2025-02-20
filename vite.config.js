ad21878c54f0fc236e42726debd55bcf42f5e970
Ahmad Shoaib Shuja <shoaibshuja829@gmail.com>
2025-02-20 22:31:06 +0430

    Add source code

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Todo-App",
   plugins: [react(), tailwindcss()],
-  base: "/my-project",
 })
